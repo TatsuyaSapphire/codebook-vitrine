c8bbb40457f8cf783a182012b0f90061a4a8ccc8
aguinot58 <aymeric.guinot@gmail.com>
2024-09-09 09:57:11 +0200

    Ajout modals pour register et choix login via Google ou mail/mdp

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { useSearch } from '../components/SearchContext';
 import { useSelector, useDispatch } from 'react-redux';
 import { update } from './../store/themeSlice';
-import { signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from 'firebase/auth';
+import { signOut, onAuthStateChanged } from 'firebase/auth';
 import { auth } from './../firebase/server.js';
 import { SearchBar } from './SearchBar';
 import logo from '../assets/logo.png';
@@ -12,6 +12,8 @@ import '../index.css';
 import './Header.css';
 import { collection, onSnapshot } from 'firebase/firestore';
 import { db } from './../firebase/server'; // Importez votre instance de Firebase Firestore
+import { LoginModal } from './LoginModal'; // Importez le composant de la modal
+import { RegisterModal } from './RegisterModal';
 
 export const Header = () => {
     const dispatch = useDispatch();
@@ -20,6 +22,8 @@ export const Header = () => {
     const { showSearchBar, setShowSearchBar } = useSearch(); // Correction ici
     const theme = useSelector((state) => state.themeState.theme);
     const [cartItemCount, setCartItemCount] = useState(0);
+    const [showLoginModal, setShowLoginModal] = useState(false);
+    const [showRegisterModal, setShowRegisterModal] = useState(false);
 
     const handleRedirect = () => {
         navigate('/products'); // Redirige vers la liste des produits
@@ -38,15 +42,24 @@ export const Header = () => {
         dispatch(update(newTheme));
     };
 
-    // Fonction pour se connecter
-    const login = async () => {
-        const provider = new GoogleAuthProvider();
-        try {
-            await signInWithPopup(auth, provider);
-            navigate('/');
-        } catch (error) {
-            console.error('Erreur de connexion : ', error);
-        }
+    // Ouvrir la modal de login
+    const handleLoginClick = () => {
+        setShowLoginModal(true);
+    };
+
+    // Fermer la modal de login
+    const handleCloseModal = () => {
+        setShowLoginModal(false);
+    };
+
+    // Ouvrir la modal de register
+    const handleRegisterClick = () => {
+        setShowRegisterModal(true);
+    };
+    
+    // Fermer la modal de register
+    const handleCloseRegisterModal = () => {
+        setShowRegisterModal(false);
     };
 
     // Fonction pour se déconnecter
@@ -125,14 +138,23 @@ export const Header = () => {
                                     </li>
                                 </>
                             ) : (
-                                <li className="dropdown-item" type="button" onClick={login}>
-                                    Login
-                                </li>
+                                <>
+                                    <li className="dropdown-item" type="button" onClick={handleLoginClick}>
+                                        Login
+                                    </li>
+                                    <li className="dropdown-item" type="button" onClick={handleRegisterClick}>
+                                        Register
+                                    </li>
+                                </>
+                                
                             )}
                         </ul>
                     </div>
                 </div>
             </nav>
+            {/* Modal de connexion */}
+            <LoginModal show={showLoginModal} handleClose={handleCloseModal} />
+            <RegisterModal show={showRegisterModal} handleClose={handleCloseRegisterModal} />
             <SearchBar />
         </header>
     );
